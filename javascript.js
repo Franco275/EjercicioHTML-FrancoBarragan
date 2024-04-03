ca53e2cfd530dd77c52d0d7145e90052dd63fb66
Franco Barragán <146877517+Franco275@users.noreply.github.com>
2024-04-03 09:18:40 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,9 +10,8 @@ let resultado = sumaNumeros(10,4);
 console.log(resultado); 
 
 function sumar(num1, num2) {
-    num1 = parseFloat(document.getElementById("num1").value);
-    num2 = parseFloat(document.getElementById("num2").value);
+    num1 = Number(document.getElementById("num1").value);
+    num2 = Number(document.getElementById("num2").value);
     var resultado = num1 + num2;
     document.getElementById("resultado").textContent = resultado;
 }
-
